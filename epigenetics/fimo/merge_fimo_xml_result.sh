00b0d40a1ede5415e5f6835458c6b1e25e918905
Jihun Jeung <jeongjihun@gm.gist.ac.kr>
2023-01-13 17:23:10 +0900

    Add files via upload

---
+++ epigenetics/fimo/merge_fimo_xml_result.sh
@@ -0,0 +1,20 @@
+# merge all multiple output files from FIMO in a one tsv
+# All output files are located in 'fimo_output' directory
+output_fileName='merge_fimo_result.xml'
+
+# iterate
+cat header_file.xml >  ${output_fileName}
+
+for file_name in $(ls fimo_output1|grep fimo_); do
+	echo ${file_name} ;
+	#cat fimo_output1/${file_name}/fimo.tsv >> merge_fimo_result_3.tsv 
+		#problem : the first header line should be removed
+	sed -n '2,$p' fimo_output1/${file_name}/fimo.xml >>  ${output_fileName}
+done
+
+
+for file_name in $(ls fimo_output2|grep fimo_); do
+        echo ${file_name} ;
+	sed -n '2,$p' fimo_output2/${file_name}/fimo.xml >>  ${output_fileName}
+done
+
